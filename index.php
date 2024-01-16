d4d5cb772322ad8706ad4d10a44802d2baa881bc
Abdul Kholid <dul.kholid@gmail.com>
2024-01-16 15:15:22 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Main template file
+ * 
+ * @package Aquila
+ */
+
+?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wordpress Theme</title>
+ </head>
+ <body>
+    <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus odit ex impedit perspiciatis quaerat quas architecto reiciendis libero voluptas doloribus adipisci tenetur, voluptatem vitae ipsum mollitia, odio fugit, commodi perferendis.</h1>
+ </body>
+ </html>
