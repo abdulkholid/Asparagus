491a2685bc04fc208a58118760d679b5d99a4e04
Abdul Kholid <dul.kholid@gmail.com>
2024-01-18 11:10:38 +0700

    feat(Asset): add bootstrap and jquery

--- functions.php
+++ functions.php
@@ -6,14 +6,24 @@
 
 function asparagus_enqueue_scripts()
 {
+    // Register Styles
     wp_register_style('style', get_stylesheet_uri());
-    wp_register_script('main', get_template_directory_uri() . '/assets/js/main.js', null, null, true);
     wp_register_style('archive-page', get_template_directory_uri() . '/assets/css/custom-page.css');
+    wp_register_style('bootstrap', get_template_directory_uri() . '/assets/src/library/bootstrap-5.3.2-dist/css/bootstrap.min.css');
+
+    // Register Scripts
+    wp_register_script('main', get_template_directory_uri() . '/assets/js/main.js', null, null, true);
+    wp_enqueue_script('bootstrap', get_template_directory_uri() . '/assets/src/library/bootstrap-5.3.2-dist/js/bootstrap.min.js', ['jquery'], null, true);
 
     if (is_archive()) {
         wp_enqueue_style('archive-page');
     }
+    // Enqueue Style
     wp_enqueue_style('style');
+    wp_enqueue_style('bootstrap');
+
+    // Enqueue Script
     wp_enqueue_script('main');
+    wp_enqueue_script('bootstrap');
 }
 add_action('wp_enqueue_scripts', 'asparagus_enqueue_scripts');
--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 
 <div class="content">
    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia inventore sint error in ratione dolorem molestiae, quasi quo iusto nesciunt explicabo fugiat a quam, quis eaque maxime soluta iste expedita.
+   <a href="#" class="btn btn-success">Mantap</a>
 </div>
 
 <?php get_footer() ?>
